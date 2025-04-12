a0f64c756b61f83df38cdb760bfd1603e0a6f317
Le Nguyen Khang <khanglngcs210650@fpt.edu.vn>
2025-04-13 06:18:18 +0700

    Release: intergrate google login 2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { MantineProvider } from "@mantine/core";
-import { RouterProvider, ScrollRestoration } from "react-router-dom";
-import router from "./routes";
-import theme from "../styles/theme";
-import "../styles/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ms from "ms";
 import { Notifications } from "@mantine/notifications";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ms from "ms";
+import { RouterProvider } from "react-router-dom";
+import "../styles/globals.css";
+import theme from "../styles/theme";
+import router from "./routes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
