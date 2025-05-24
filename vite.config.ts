a6cd4e9f43f6802cc53140e7e00f229cb734fc76
Le Nguyen Khang <khanglngcs210650@fpt.edu.vn>
2025-05-24 18:25:36 +0700

    Backup: Implementing https and before refactoring cart page and checkout page to avoid using useEffect

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
+import path from "path";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+    },
+    port: 5173,
+  },
 });
