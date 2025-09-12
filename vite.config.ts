3d5fd5fac2b3902740ebf48e434e2b752263d827
Le Nguyen Khang <khanglngcs210650@fpt.edu.vn>
2025-09-12 10:42:32 +0700

    chore: update dependencies and add image optimization plugin
    - Added sharp and svgo for image processing.
    - Included vite-plugin-image-optimizer for optimizing images in the Vite build process.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import fs from "fs";
 import path from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer({})],
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
